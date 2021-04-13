994213fc69950b62ca2b961cff7bb1a50deaa39a
Zepeta <aggio_we@outlook.com>
2021-04-13 16:58:55 -0500

    Cambios

--- practica1.py
+++ practica1.py
@@ -2,15 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import tkinter as tk
-from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter import simpledialog, messagebox
 import matplotlib.animation as animation
+import sys
 
-fig = plt.figure(figsize=(5,5))
+fig = plt.figure(figsize=(5.5,5.5))
 ax1 = fig.add_subplot()
 root = tk.Tk()
 root.title("Práctica 1")
+
+
 
 def definirMapa():
     f = open("doc.txt","r")
@@ -34,6 +36,8 @@ def definirMapa():
     ax1.set_xticks(np.arange(1,col+1))
     ax1.set_yticks(np.arange(1,row+1))
     ax1.imshow(mapa,cmap = ListedColormap(['gray','w']), extent = [1,col+1,row+1,1])
+    plt.text(1.2,10.7,"I")
+    plt.text(1.5,10.7,"V")
     return mapa
 
 def animate(i):
@@ -51,20 +55,76 @@ def muestraMapa():
 
     canvas.get_tk_widget().pack()
     
-    ani = animation.FuncAnimation(fig, animate, interval=1000) 
+    ani = animation.FuncAnimation(fig, animate, interval=1000,cache_frame_data=False) 
 
-    btnMC = tk.Button(root_panel,
+    btn_MC = tk.Button(root_panel,
                      text = "Mostrar valor de Casilla",
                      command= lambda: mostrarCasilla(definirMapa()))    
-    btnMC.pack(padx=5, pady=10, side=tk.LEFT)
-    
-    btnCC = tk.Button(root_panel,
+    btn_MC.pack(padx=5, pady=10, side=tk.LEFT)
+
+    btn_CC = tk.Button(root_panel,
                      text = "Cambiar valor de Casilla",
                      command= lambda: cambiarCasilla(definirMapa()))       
-    btnCC.pack(padx=5, pady=10, side=tk.LEFT)
-
+    btn_CC.pack(padx=5, pady=10, side=tk.LEFT)
+    
+    root.bind("<Key>",eventoClick)
+    
     
     root.mainloop() 
+    
+columna = 0
+fila = 9
+def eventoClick(event):
+    global columna
+    global fila
+    mapa = definirMapa()    
+    print(columna+1,fila+1)
+    posicion_inicial = mapa[fila,columna]
+    posicion_actual = posicion_inicial
+    siguiente_posicion = []
+    if event.keysym == "Up":
+        fila -= 1
+        siguiente_posicion = mapa[fila,columna]
+        if  siguiente_posicion == 1:
+            posicion_actual = siguiente_posicion
+            plt.text(float(columna+1)+0.5,float(fila+1)+0.7,"V")
+        else: 
+            fila +=1
+    elif event.keysym == "Down":
+        fila +=1
+        siguiente_posicion = mapa[fila,columna]
+        if  siguiente_posicion == 1:
+            posicion_actual = siguiente_posicion
+            plt.text(float(columna+1)+0.5,float(fila+1)+0.7,"V")
+        else:
+            fila -= 1
+    elif event.keysym == "Right":
+        columna +=1
+        siguiente_posicion = mapa[fila,columna]
+        if  siguiente_posicion == 1:
+            posicion_actual = siguiente_posicion
+            plt.text(float(columna+1)+0.5,float(fila+1)+0.7,"V")
+        else:
+            columna -= 1
+    elif event.keysym == "Left":
+        columna -=1
+        siguiente_posicion = mapa[fila,columna]
+        if  siguiente_posicion == 1:
+            posicion_actual = siguiente_posicion
+            plt.text(float(columna+1)+0.5,float(fila+1)+0.7,"V")
+        else:
+            columna += 1
+    elif event.keysym == "Escape":
+        root.destroy()
+        sys.exit()
+
+
+def calcularPosicion():
+    mapa = definirMapa()    
+    posicion_inicial = mapa[9,0]
+    
+    
+    
 
 def mostrarCasilla(mapa):
     valorCol = simpledialog.askinteger("Columna","Ingrese el valor de la columna deseada", parent=root) 
